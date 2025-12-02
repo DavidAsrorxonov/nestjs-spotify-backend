49f6354320ca372d6e8dfd1184b3c797452318dc
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-12-02 14:36:25 +0900

    add CRUD operations for songs in SongsController and update REST API tests

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post()
+  create() {
+    return 'New song is created';
+  }
+
   @Get()
   findAll() {
     return 'All songs fetched';
+  }
+  @Get(':id')
+  findOne() {
+    return `One song is fetched`;
+  }
+  @Put(':id')
+  updateOne() {
+    return `One song is updated`;
+  }
+  @Delete(':id')
+  deleteOne() {
+    return `One song is deleted`;
   }
 }
